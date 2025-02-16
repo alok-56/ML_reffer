1ca812299ebc506c391ea8c35bd5a6f0e6dbff59
alok <your_email@example.com>
2025-02-17 03:01:02 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from "react-router-dom";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 ModuleRegistry.registerModules([AllCommunityModule]);
-import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <Router>
-        <App />
-      </Router>
-    </HashRouter>
-  </StrictMode>
-);
+    
+    <Router>
+    <App />
+  </Router>
+  </StrictMode>,
+)
